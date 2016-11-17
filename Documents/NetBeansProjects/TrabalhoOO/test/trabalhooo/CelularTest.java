39679c3f9bef0655bde93656a00dff69874c5744
mathiasVoelcker <mathias_V94@hotmail.com>
2016-11-17 20:12:38 -0200

    Criar testes enviar e receber mensagem de celular

--- Documents/NetBeansProjects/TrabalhoOO/test/trabalhooo/CelularTest.java
+++ Documents/NetBeansProjects/TrabalhoOO/test/trabalhooo/CelularTest.java
@@ -5,6 +5,7 @@
  */
 package trabalhooo;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -21,22 +22,7 @@ public class CelularTest {
     
     public CelularTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
+
 
     /**
      * Test of enviarMensagem method, of class Celular.
@@ -44,98 +30,70 @@ public class CelularTest {
     @Test
     public void testEnviarMensagem() throws Exception {
         System.out.println("enviarMensagem");
-        String numCelularDesejado = "";
-        String mensagemTexto = "";
-        Central central = null;
-        Celular instance = null;
-        instance.enviarMensagem(numCelularDesejado, mensagemTexto, central);
+        List<Celular> celulares = new ArrayList<Celular>();
+        List<Antena> antenas = new ArrayList<Antena>();
+        Antena a1 = new Antena("antena1", 4, 4000, 5);
+        antenas.add(a1);
+        Celular c1 = new Celular("99994438", a1);
+        celulares.add(c1);
+        Celular c2 = new Celular("99932438", a1);
+        celulares.add(c2);
+        String numCelularDesejado = c2.getNumero();
+        String mensagemTexto = "Mensagem de teste1";
+        
+        Central central = new Central(celulares, antenas);
+        
+        c1.enviarMensagem(numCelularDesejado, mensagemTexto, central);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals("Mensagem de teste1", c2.getUltimaMensagem());
     }
 
-    /**
-     * Test of procurarAntena method, of class Celular.
-     */
-    @Test
-    public void testProcurarAntena() {
-        System.out.println("procurarAntena");
-        List<Antena> antenas = null;
-        Celular instance = null;
-        instance.procurarAntena(antenas);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of receberMensagem method, of class Celular.
-     */
+
+//    /**
+//     * Test of receberMensagem method, of class Celular.
+//     */
     @Test
     public void testReceberMensagem() {
         System.out.println("receberMensagem");
-        String mensagem = "";
-        String numCelularEnviando = "";
-        Celular instance = null;
-        instance.receberMensagem(mensagem, numCelularEnviando);
+        String mensagem = "Mensagem de teste2";
+        String numCelularEnviando = "99932543";
+        Celular c1 = new Celular("99994438", new Antena("antena1", 4, 4000, 5));
+        c1.receberMensagem(mensagem, numCelularEnviando);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(mensagem, c1.getUltimaMensagem());
     }
+//
+//    /**
+//     * Test of getNumero method, of class Celular.
+//     */
+//    @Test
+//    public void testGetNumero() {
+//        System.out.println("getNumero");
+//        Celular instance = null;
+//        String expResult = "";
+//        String result = instance.getNumero();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//
+//
+//    /**
+//     * Test of getAntenaMaisProxima method, of class Celular.
+//     */
+//    @Test
+//    public void testGetAntenaMaisProxima() {
+//        System.out.println("getAntenaMaisProxima");
+//        Celular instance = null;
+//        Antena expResult = null;
+//        Antena result = instance.getAntenaMaisProxima();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
-    /**
-     * Test of getNumero method, of class Celular.
-     */
-    @Test
-    public void testGetNumero() {
-        System.out.println("getNumero");
-        Celular instance = null;
-        String expResult = "";
-        String result = instance.getNumero();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of getLocalizacao method, of class Celular.
-     */
-    @Test
-    public void testGetLocalizacao() {
-        System.out.println("getLocalizacao");
-        Celular instance = null;
-        int expResult = 0;
-        int result = instance.getLocalizacao();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getAntenaMaisProxima method, of class Celular.
-     */
-    @Test
-    public void testGetAntenaMaisProxima() {
-        System.out.println("getAntenaMaisProxima");
-        Celular instance = null;
-        Antena expResult = null;
-        Antena result = instance.getAntenaMaisProxima();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of distancia method, of class Celular.
-     */
-    @Test
-    public void testDistancia() {
-        System.out.println("distancia");
-        int local1 = 0;
-        int local2 = 0;
-        Celular instance = null;
-        int expResult = 0;
-        int result = instance.distancia(local1, local2);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
+
